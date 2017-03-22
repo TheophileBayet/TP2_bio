39caa5bac238b2883c3c074d4e0cb128daedcb47
Léo Milhade <leo.milhade@ensimag.grenoble-inp.fr>
2017-03-22 09:12:09 +0100

    début des corrections

--- src/swOut.c
+++ src/swOut.c
@@ -10,44 +10,54 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2)
     unsigned int i = mat->h-1; // -1 ?
     unsigned int j = mat->w-1; // same
 
-    char *s1_res = ""; // résultat avec letters et '-' pour s1
-    char *s2_res = ""; // same s2
+    char s1_res[sizeof(s1)+sizeof(s2)]; // résultat avec letters et '-' pour s1
+    char s2_res[sizeof(s1)+sizeof(s2)]; // résultat avec letters et '-' pour s1
 
     while (i!=0 || j!=0){
 
-      //un seul chemin, on privilégie la diag (prevs&1) (arbitraire)
-      // à changer pour tous les chemins
+        //un seul chemin, on privilégie la diag (prevs&1) (arbitraire)
+        // à changer pour tous les chemins
 
-      struct cell cur = mat->cells[i*mat->w+j];
+        struct cell cur = mat->cells[i*mat->w+j];
 
-      // màj des chemins selon déplacement optimal trouvé
-      
-      switch(cur.prevs){
+        printf("Résultat intermédiaire : %.*s\n", mat->h-1 + mat->w-1 - j+i, s1_res + j+i);
+        // màj des chemins selon déplacement optimal trouvé
 
-	//diag
-      case 1 :
-	s1_res = strcat(s1[j-1], s1_res);
-	s2_res = strcat(s2[i-1], s2_res);
-	i--; j--;
-	break;
+        switch(cur.prevs){
 
-      case 2 :
-	s1_res = strcat(s1[j-1],s1_res);
-	s2_res = strcat("-",s2_res);
-	j--;
-	break;
+            //diag
+            case 1 :
+                printf("Case 1\n");
+                s1_res[i+j] = s1[j-1];
+                s2_res[i+j] = s2[i-1];
+                //s1_res = strcat(s1[j-1], s1_res);
+                //s2_res = strcat(s2[i-1], s2_res);
+                i--; j--;
+                break;
 
-      case 4 :
-	s1_res = strcat("-",s1_res);
-	s2_res = strcat(s2[i-1],s2_res);
-	i--;
-	break;
+            case 2 :
+                printf("Case 2\n");
+                s1_res[i+j] = s1[j-1];
+                s2_res[i+j] = '-';
+                //s1_res = strcat(s1[j-1],s1_res);
+                //s2_res = strcat("-",s2_res);
+                j--;
+                break;
 
-      default :
-	fprintf(stderr, "%s\n", "Pas de cas correspondant pour prevs");
-        exit(1);
+            case 4 :
+                printf("Case 4\n");
+                s1_res[i+j] = '-';
+                s2_res[i+j] = s2[i-1];
+                //s1_res = strcat("-",s1_res);
+                //s2_res = strcat(s2[i-1],s2_res);
+                i--;
+                break;
 
-      }
+            default :
+                fprintf(stderr, "%s\n", "Pas de cas correspondant pour prevs");
+                exit(1);
+
+        }
     }
-    
+
 }
