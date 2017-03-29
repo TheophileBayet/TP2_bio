bbe4ee887e8de5adc1e0f8f8d5a3908899883616
Gladys Baudet <gladys.baudet@ensimag.grenoble-inp.fr>
2017-03-29 10:49:37 +0200

    correction affichage indel

--- src/swOut.c
+++ src/swOut.c
@@ -79,15 +79,16 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2)
 
             case 2 :
             case 6 :
-                s1_res[count] = s1[j-1] + 32;
-                s2_res[count] = '-';
-                j--;
-                break;
 
-            case 4 :
                 s1_res[count] = '-';
                 s2_res[count] = s2[i-1] + 32;
                 i--;
+                break;
+
+            case 4 :
+            s1_res[count] = s1[j-1] + 32;
+            s2_res[count] = '-';
+            j--;
                 break;
 
             default :
@@ -168,15 +169,15 @@ void printBestAlisGotoh(struct matrix *D, struct matrix *V, struct matrix *H, /*
 
             case 2 :
             case 6 :
-                s1_res[count] = s1[j-1];
-                s2_res[count] = '-';
-                j--;
+            s1_res[count] = '-';
+            s2_res[count] = s2[i-1] + 32;
+            i--;
                 break;
 
             case 4 :
-                s1_res[count] = '-';
-                s2_res[count] = s2[i-1];
-                i--;
+            s1_res[count] = s1[j-1] + 32;
+            s2_res[count] = '-';
+            j--;
                 break;
 
             default :
